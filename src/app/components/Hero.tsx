cf590446c171fc73d3dfef6bdffeba33d85c1f06
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-09-20 14:37:06 +0500

    Updated website and add logo

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,25 +1,42 @@
 import React from 'react'
+// import { Fade } from "react-reveal";
 import Image from 'next/image'
 const Hero = () => {
   return (
-    <div id="Hero" className="w-full h-screen">
+    <div id="Hero" className=" w-full h-screen">
+      {/* Container */}
+      {/* <Fade cascade bottom> */}
         <div
-          className={` w-full h-full m-auto flex justify-center items-start flex-col p-4`} >
-          {/* .............>> Image ................*/}
-           <div className="relative  flex items-start justify-start w-[85px] h-[100.68px] ">
-            <Image src={"/BG/logo11.png"}  layout="fill" objectFit="contain" alt='logo' />
+          className={`max-w-[1380px] w-full h-full m-auto flex justify-start items-start flex-col p-4`} >
+          {/* Image */}
+            <div className="relative w-[70px]  h-[70px] mb-7 mr-6 md:w-[219.21px] sm:h-[139.68px]">
+            <Image src={"/BG/Logo11.png"} layout="fill" objectFit="contain" />
           </div>
-          {/* .....................>>>>  Heading ..................*/}
-          <h1 className="font-Ubuntu font-bold text-white md:text-[65px] sm:text-[50px] text-[40px] md:leading-[70px] sm:leading-[58px] leading-[46px] mt-24 mb-12">
+
+
+
+          {/* <div className="relative w-[83.77px] h-[88px]">
+            <Image
+              src={"/BG/Logo.png"}
+              layout="fill"
+              objectFit="contain"
+              alt="logo"
+            />
+          </div> */}
+
+          {/* Heading */}
+          <h1 className="font-Ubuntu font-bold text-white md:text-[65px] sm:text-[50px] text-[44px] md:leading-[70px] sm:leading-[58px] leading-[46px] mt-24 mb-12">
             Where
             <br /> Decentralized
             <br /> Finance meets
             <br /> Gambling
           </h1>
-          <button className="w-[250px] h-[65px] bg-pink from-pink to-blue hover:animate-bounce text-white text-[16px] leading-[18px] rounded">
+
+          <button className=" w-[180px] sm:w-[250px] h-[45px] sm:h-[65px] bg-pink from-pink to-blue hover:animate-bounce text-white text-[16px] leading-[18px] rounded">
             Launch App
           </button>
         </div>
+      {/* </Fade> */}
     </div>
   )
 }
